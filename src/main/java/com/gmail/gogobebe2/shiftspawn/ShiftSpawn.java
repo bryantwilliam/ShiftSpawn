b94b2fb907c4a8525a323e7fb144a43034dcef99
gogobebe2 <gogobebe2@gmail.com>
2015-05-21 07:21:50 +1200

    1.0.1-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftspawn/ShiftSpawn.java
+++ src/main/java/com/gmail/gogobebe2/shiftspawn/ShiftSpawn.java
@@ -1,6 +1,27 @@
 package com.gmail.gogobebe2.shiftspawn;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShiftSpawn extends JavaPlugin {
+    public final static String MIN_PLAYERS_KEY = "Minimum players before game starts";
+    public final static String TIME_BEFORE_START_KEY = "Time before games starts";
+    public final static String GAME_TIME = "Game time";
+    @Override
+    public void onEnable() {
+        getLogger().info("Starting up ShiftSpawn. If you need me to update this plugin, email at gogobebe2@gmail.com");
+        saveDefaultConfig();
+        Timer timer = new Timer(this);
+        if (Bukkit.getOnlinePlayers().size() > getConfig().getInt(MIN_PLAYERS_KEY)) {
+            timer.setGameState(GameState.STARTING);
+            timer.setTime(getConfig().getString(TIME_BEFORE_START_KEY));
+        }
+
+        timer.startTimer(false);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabling ShiftSpawn. If you need me to update this plugin, email at gogobebe2@gmail.com");
+    }
 }
---
+++ src/main/java/com/gmail/gogobebe2/shiftspawn/Timer.java
@@ -0,0 +1,133 @@
+package com.gmail.gogobebe2.shiftspawn;
+
+import be.maximvdw.featherboard.api.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.regex.PatternSyntaxException;
+
+public class Timer {
+    private int minutes = 0;
+    private int seconds = 0;
+    private boolean isTimerRunning = false;
+    private ShiftSpawn plugin;
+    private BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+    private int timerIncrementer;
+    private GameState gameState;
+
+    public Timer(ShiftSpawn plugin) {
+        this(plugin, GameState.WAITING);
+    }
+
+    public Timer(ShiftSpawn plugin, GameState gameState) {
+        this(plugin, gameState, "0:00");
+    }
+
+    public Timer(ShiftSpawn plugin, GameState gameState, String timeFormat) {
+        this.plugin = plugin;
+        this.gameState = gameState;
+        setTime(timeFormat);
+    }
+
+    public void setTime(String timeFormat) throws NumberFormatException, PatternSyntaxException {
+        String[] times = timeFormat.split(":");
+        setMinutes(Integer.parseInt(times[0]));
+        setSeconds(Integer.parseInt(times[1]));
+    }
+
+    public String getTime() {
+        if (seconds < 10) {
+            return getMinutes() + ":0" + getSeconds();
+        }
+        return getMinutes() + ":" + getSeconds();
+    }
+
+    public void startTimer(final boolean goUp) {
+        if (!isTimerRunning()) {
+            this.timerIncrementer = scheduler.scheduleSyncRepeatingTask(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    PlaceholderAPI.registerOfflinePlaceholder("shiftspawn", true,
+                            new PlaceholderAPI.PlaceholderRequestEventHandler() {
+                                @Override
+                                public String onPlaceholderRequest(PlaceholderAPI.PlaceholderRequestEvent e) {
+                                    String prefix;
+                                    switch (gameState) {
+                                        case WAITING:
+                                            return ChatColor.BLUE + "Waiting." + (seconds % 2 == 0 ? ".." : ".");
+                                        case STARTING:
+                                            prefix = ChatColor.DARK_AQUA + "Starting in: " + ChatColor.AQUA;
+                                            break;
+                                        case STARTED:
+                                            prefix = ChatColor.GREEN + "Time left: " + ChatColor.DARK_GREEN;
+                                            break;
+                                        default:
+                                            prefix = ChatColor.RED + "Error! " + ChatColor.RESET;
+                                            break;
+                                    }
+                                    return prefix + getTime();
+                                }
+                            });
+                    if (goUp) {
+                        seconds++;
+                        if (seconds == 60) {
+                            minutes++;
+                            seconds = 0;
+                        }
+                    } else {
+                        if (seconds != 0 || minutes != 0) {
+                            seconds--;
+                            if (seconds == -1) {
+                                minutes--;
+                                seconds = 59;
+                            } else if (seconds == 0 && minutes == 0) {
+                                stopTimer();
+                            }
+                        }
+                    }
+                }
+            }, 0L, 20L);
+            this.isTimerRunning = true;
+        }
+    }
+
+    public void startTimer() {
+        startTimer(true);
+    }
+
+    public void stopTimer() {
+        if (isTimerRunning) {
+            scheduler.cancelTask(this.timerIncrementer);
+            this.isTimerRunning = false;
+        }
+    }
+
+    public int getMinutes() {
+        return this.minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return this.seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public boolean isTimerRunning() {
+        return this.isTimerRunning;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(GameState gameState) {
+        this.gameState = gameState;
+    }
+}
