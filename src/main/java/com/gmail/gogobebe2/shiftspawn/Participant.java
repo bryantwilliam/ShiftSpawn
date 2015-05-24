3c50b023efbc452eafb5d60df3b6e1cc92accd3a
gogobebe2 <gogobebe2@gmail.com>
2015-05-25 07:23:58 +1200

    2.0.8-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftspawn/Participant.java
+++ src/main/java/com/gmail/gogobebe2/shiftspawn/Participant.java
@@ -3,7 +3,37 @@ package com.gmail.gogobebe2.shiftspawn;
 import org.bukkit.entity.Player;
 
 public class Participant {
-    private Player player;
+    private final Player PLAYER;
     private int score;
     private String spawnID;
+
+    public Participant(final Player PLAYER, String spawnID) {
+        this(PLAYER, spawnID, 0);
+    }
+
+    public Participant(final Player PLAYER, String spawnID, int score) {
+        this.PLAYER = PLAYER;
+        this.spawnID = spawnID;
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Player getPlayer() {
+        return PLAYER;
+    }
+
+    public String getSpawnID() {
+        return spawnID;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setSpawnID(String spawnID) {
+        this.spawnID = spawnID;
+    }
 }
