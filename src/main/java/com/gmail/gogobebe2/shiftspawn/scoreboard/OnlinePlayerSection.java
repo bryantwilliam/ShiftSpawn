0b142c6e93b66b95f46689d955bfc091597bf9ad
gogobebe2 <gogobebe2@gmail.com>
2015-06-03 13:06:10 +1200

    2.9.2-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftspawn/scoreboard/OnlinePlayerSection.java
+++ src/main/java/com/gmail/gogobebe2/shiftspawn/scoreboard/OnlinePlayerSection.java
@@ -7,12 +7,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.scoreboard.Objective;
 
 public class OnlinePlayerSection extends ScoreboardSection {
+    private ShiftSpawn plugin;
     public OnlinePlayerSection(Participant participant, Objective objective, ShiftSpawn plugin) {
         super(participant, objective, plugin);
+        this.plugin = plugin;
     }
 
     @Override
     public void arrangeSection() {
-        setHeading(ChatColor.AQUA + "" + ChatColor.ITALIC + "Players online: " + ChatColor.DARK_AQUA + Bukkit.getOnlinePlayers().size(), 0);
+        setHeading(ChatColor.AQUA + "" + ChatColor.ITALIC + "Players online: " + ChatColor.DARK_AQUA + Bukkit.getOnlinePlayers().size() + "/" + plugin.getConfig().getInt(ShiftSpawn.MIN_PLAYERS_KEY), 0);
     }
 }
