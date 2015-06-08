b45638ce8ee0877d742e8a5a142c322f80afa770
gogobebe2 <gogobebe2@gmail.com>
2015-06-08 17:02:32 +1200

    1.8.0-PREALPHA-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftspawn/scoreboard/TopScoresSection.java
+++ src/main/java/com/gmail/gogobebe2/shiftspawn/scoreboard/TopScoresSection.java
@@ -2,7 +2,6 @@ package com.gmail.gogobebe2.shiftspawn.scoreboard;
 
 import com.gmail.gogobebe2.shiftspawn.Participant;
 import com.gmail.gogobebe2.shiftspawn.ShiftSpawn;
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
@@ -13,7 +12,6 @@ import java.util.List;
 
 public class TopScoresSection extends ScoreboardSection {
     private List<Score> scores = new ArrayList<>();
-    private Score subHeading = null;
 
     public TopScoresSection(Participant participant, Objective objective, ShiftSpawn plugin) {
         super(participant, objective, plugin);
@@ -32,29 +30,20 @@ public class TopScoresSection extends ScoreboardSection {
         Participant[] participants = getPlugin().getParticipants().toArray(new Participant[getPlugin().getParticipants().size()]);
         Arrays.sort(participants);
 
-        setLabel(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Scores:", participants.length + 2);
+        setLabel(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Scores: "
+                + ChatColor.DARK_GREEN + "  " + ChatColor.ITALIC + "score" +
+                ChatColor.DARK_RED + "  " + ChatColor.ITALIC + "kills", participants.length + 1);
 
-        if (subHeading == null) {
-            subHeading = getObjective().getScore(getAlignedText(
-                    ChatColor.DARK_GREEN + StringUtils.repeat(" ", 19) + ChatColor.ITALIC + "score",
-                    ChatColor.DARK_RED + "" + ChatColor.ITALIC + "kills", 40));
-            subHeading.setScore(participants.length + 1);
-        }
 
         for (int pIndex = 0; pIndex < participants.length; pIndex++) {
             Participant participant = participants[pIndex];
 
-            String s = ChatColor.DARK_GREEN + "" + ChatColor.BOLD + participant.getScore();
-            String p = ChatColor.DARK_PURPLE + " " + participant.getPlayer().getName() + ":";
-            String prefix = getAlignedText(p, s, (ChatColor.DARK_GREEN + StringUtils.repeat(" ", 19) + ChatColor.ITALIC).length());
-            String suffix = ChatColor.DARK_RED + "" + ChatColor.BOLD + participant.getKills();
-            Score score = getObjective().getScore(getAlignedText(prefix, suffix, 40));
+            Score score = getObjective().getScore(
+                    ChatColor.DARK_PURPLE + " " + participant.getPlayer().getName() + ":  "
+                            + ChatColor.DARK_GREEN + ChatColor.BOLD + participant.getScore()
+                            + "  " + ChatColor.DARK_RED + ChatColor.BOLD + participant.getKills());
             score.setScore(pIndex + 1);
             scores.add(score);
         }
-    }
-
-    private String getAlignedText(String prefix, String suffix, int charLimit) {
-        return prefix + StringUtils.repeat(" ", charLimit - prefix.length() - suffix.length()) + suffix;
     }
 }
