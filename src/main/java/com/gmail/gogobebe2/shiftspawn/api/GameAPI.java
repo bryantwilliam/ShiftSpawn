97e8b532ed062fd21bb03e01720247235437f3fd
gogobebe2 <gogobebe2@gmail.com>
2015-12-25 13:03:25 +1300

    1.4.0-BETA-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftspawn/api/GameAPI.java
+++ src/main/java/com/gmail/gogobebe2/shiftspawn/api/GameAPI.java
@@ -2,6 +2,12 @@ package com.gmail.gogobebe2.shiftspawn.api;
 
 import com.gmail.gogobebe2.shiftspawn.GameState;
 import com.gmail.gogobebe2.shiftspawn.ShiftSpawn;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+import java.util.UUID;
 
 @SuppressWarnings("unused") // It's an API so is not locally used.
 public final class GameAPI {
@@ -22,5 +28,28 @@ public final class GameAPI {
 
     public static int getMinutes() {
         return ShiftSpawn.getInstance().getGame().getMinutes();
+    }
+
+    public static UUID getMapUUID() {
+        ShiftSpawn plugin = ShiftSpawn.getInstance();
+        World world = null;
+        for (String idStr : plugin.getConfig().getConfigurationSection("Spawns").getKeys(false)) {
+            try {
+                world = Bukkit.getWorld(plugin.getConfig().getString("Spawns." + Integer.parseInt(idStr) + ".World"));
+                break;
+            } catch (NullPointerException ex) {
+                ex.printStackTrace();
+            }
+        }
+        assert world != null;
+        return world.getUID();
+    }
+
+    public static int getMaxPlayers() {
+        return Bukkit.getMaxPlayers();
+    }
+
+    public static Collection<? extends Player> getPlayers() {
+        return Bukkit.getOnlinePlayers();
     }
 }
